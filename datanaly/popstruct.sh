a55e3b5e91e28c78c9f41e7b46bf1ad435172705
esohkevin <esohkevin4@gmail.com>
2019-01-14 14:22:43 +0300

    updates

--- datanaly/popstruct.sh
+++ datanaly/popstruct.sh
@@ -8,163 +8,162 @@
 #		--maf 0.35 \
 #		--geno 0.01 \
 #		--biallelic-only \
-#		--recode \
+#		--make-bed \
 #		--allow-no-sex \
 #		--out ${i}
 #done
 
 plink \
-	--file 1000G/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--merge 1000G/ALL.chr2.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile 1000G/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--bmerge 1000G/ALL.chr2.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge1
 
 plink \
-	--file merge1 \
-	--merge 1000G/ALL.chr3.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge1 \
+	--bmerge 1000G/ALL.chr3.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge2
 
 plink \
-	--file merge2 \
-	--merge 1000G/ALL.chr4.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge2 \
+	--bmerge 1000G/ALL.chr4.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge3
 
 plink \
-	--file merge3 \
-	--merge 1000G/ALL.chr5.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge3 \
+	--bmerge 1000G/ALL.chr5.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge4
 
 plink \
-	--file merge4 \
-	--merge 1000G/ALL.chr6.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge4 \
+	--bmerge 1000G/ALL.chr6.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge5
 
 plink \
-	--file merge5 \
-	--merge 1000G/ALL.chr7.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge5 \
+	--bmerge 1000G/ALL.chr7.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge6
 
 plink \
-	--file merge6 \
-	--merge 1000G/ALL.chr8.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge6 \
+	--bmerge 1000G/ALL.chr8.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge7
 
 plink \
-	--file merge7 \
-	--merge 1000G/ALL.chr9.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge7 \
+	--bmerge 1000G/ALL.chr9.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge8
 
 plink \
-	--file merge8 \
-	--merge 1000G/ALL.chr10.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge8 \
+	--bmerge 1000G/ALL.chr10.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge9
 
 plink \
-	--file merge9 \
-	--merge 1000G/ALL.chr11.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge9 \
+	--bmerge 1000G/ALL.chr11.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge10
 
 plink \
-	--file merge10 \
-	--merge 1000G/ALL.chr12.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge10 \
+	--bmerge 1000G/ALL.chr12.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge11
 
 plink \
-	--file merge11 \
-	--merge 1000G/ALL.chr13.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge11 \
+	--bmerge 1000G/ALL.chr13.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge12
 
 plink \
-	--file merge12 \
-	--merge 1000G/ALL.chr14.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge12 \
+	--bmerge 1000G/ALL.chr14.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge13
 
 plink \
-	--file merge13 \
-	--merge 1000G/ALL.chr15.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge13 \
+	--bmerge 1000G/ALL.chr15.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge14
 
 plink \
-	--file merge14 \
-	--merge 1000G/ALL.chr16.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge14 \
+	--bmerge 1000G/ALL.chr16.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge15
 
 plink \
-	--file merge15 \
-	--merge 1000G/ALL.chr17.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge15 \
+	--bmerge 1000G/ALL.chr17.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge16
 
 plink \
-	--file merge16 \
-	--merge 1000G/ALL.chr18.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge16 \
+	--bmerge 1000G/ALL.chr18.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge17
 
 plink \
-	--file merge17 \
-	--merge 1000G/ALL.chr19.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge17 \
+	--bmerge 1000G/ALL.chr19.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge18
 
 plink \
-	--file merge18 \
-	--merge 1000G/ALL.chr20.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge18 \
+	--bmerge 1000G/ALL.chr20.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge19
 
 plink \
-	--file merge19 \
-	--merge 1000G/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge19 \
+	--bmerge 1000G/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merge20
 
 plink \
-	--file merge20 \
-	--merge 1000G/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-	--recode \
+	--bfile merge20 \
+	--bmerge 1000G/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+	--make-bed \
 	--biallelic-only \
 	--out merged1kp3
 #mv merged1kp3.* .
 
 plink \
-	--file merged1kp3 \
+	--bfile merged1kp3 \
 	--thin-indiv-count 360 \
-	--make-bed \
 	--autosome \
 	--exclude-snp rs16959560 \
 	--keep-allele-order \
@@ -212,7 +211,7 @@ plink \
 	--cluster --mds-plot 2 \
 	--out merge
 
-# Retrieve the 1000Genome Phase sample file from the website by visiting
+# Retrieve the 1000Genome Phase sample bfile from the website by visiting
 # http://www.internationalgenome.org/data-portal/sample
 # Then click on Phase 3 to filter and then hit the button to download the list
 
@@ -222,11 +221,11 @@ sed 's/ /_/g' igsr_samples.tsv > igsr_phase3.samples
 # Extract ids of the 360 randomly generated phase 3 samples
 cut -f1 -d' ' 1kGp3.fam > 3601kGp3.ids
 
-# Now obtain the ids and their corresponding populations in the igsr_phase3.samples file
+# Now obtain the ids and their corresponding populations in the igsr_phase3.samples bfile
 echo "IID Status" > merge.txt
 grep -f 3601kGp3.ids igsr_phase3.samples | cut -f1,4 >> merge.txt
 
-# Also obtain the ids of the remaining dataset in psdata.fam and corresponding status in the large sample file
+# Also obtain the ids of the remaining dataset in psdata.fam and corresponding status in the large sample bfile
 cut -f1 -d' ' psdata.fam > psdata.ids
 grep -f psdata.ids tmp/Cameroon_GWAS-2.5M_b37_release.sample | cut -f1,9 -d' ' >> merge.txt
 
