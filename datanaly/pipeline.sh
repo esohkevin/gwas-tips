faa10d9fd88ca3cc193c17f933c6b5b9dfdf26b7
esohkevin <esohkevin4@gmail.com>
2019-01-16 09:58:49 +0300

    updates

--- datanaly/pipeline.sh
+++ datanaly/pipeline.sh
@@ -239,23 +239,18 @@ plink1.9 \
 	--out autopseudo
 cat autopseudo.log >> all.log
 
-#########################################################################
-#                        Plot Association in R                          #
-#########################################################################
-echo -e "\nNow generating association plots in R. Please wait..."
-
-R CMD BATCH assocplot.R
-
 ### Association tests (all chromosomes) including covariats to account for population structure
 # With PC1 and PC2
 plink \
 	--bfile qc-camgwas \
 	--covar ps-data.mds \
 	--covar-name C1 C2 \
+	--autosome \
 	--allow-no-sex \
 	--hide-covar \
 	--logistic \
 	--out ps-qc-camgwas
+cat ps-qc-camgwas.log >> all.log
 
 # With PC1, PC5 and PC9 as reported by glm to associate significantly with disease
 plink \
@@ -263,9 +258,11 @@ plink \
         --covar ps-data.mds \
         --covar-name C1 C5 C9 \
         --allow-no-sex \
+	--autosome \
         --hide-covar \
         --logistic \
         --out ps1-qc-camgwas
+cat ps1-qc-camgwas.log >> all.log
 
 # With all PCs
 plink \
@@ -273,9 +270,19 @@ plink \
         --covar ps-data.mds \
         --covar-name C1 C2 C3 C4 C5 C6 C7 C8 C9 C10 \
         --allow-no-sex \
+	--autosome \
         --hide-covar \
         --logistic \
         --out ps2-qc-camgwas
+cat ps2-qc-camgwas.log >> all.log
+
+#########################################################################
+#                        Plot Association in R                          #
+#########################################################################
+echo -e "\nNow generating association plots in R. Please wait..."
+
+R CMD BATCH assocplot.R
+
 
 #done
 
